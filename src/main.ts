69c0144966916aa9e0766afbe8dc79d113c62f12
Abdelqader <saabood519@gmail.com>
2026-05-08 15:36:04 +0300

    fix: email service3

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import type { Request, Response } from 'express';
+import * as dns from 'node:dns';
+
+dns.setDefaultResultOrder('ipv4first');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
